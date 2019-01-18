6e97b5e5fc4f3b886bc09a520dc33cfa05cf5073
Sergey Samoylov <sergey.samoylov@spryker.com>
2019-01-18 14:11:18 +0200

    ps-6931: move currency validation to currency module, small fixes

---
+++ src/Spryker/Zed/Currency/Communication/Plugin/Quote/QuoteCurrencyValidatePlugin.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Currency\Communication\Plugin\Quote;
+
+use Generated\Shared\Transfer\MessageTransfer;
+use Generated\Shared\Transfer\QuoteTransfer;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+use Spryker\Zed\QuoteExtension\Dependency\Plugin\QuoteValidatePluginInterface;
+
+/**
+ * @method \Spryker\Zed\Currency\Business\CurrencyFacade getFacade()
+ * @method \Spryker\Zed\Currency\Communication\CurrencyCommunicationFactory getFactory()
+ * @method \Spryker\Zed\Currency\CurrencyConfig getConfig()
+ * @method \Spryker\Zed\Currency\Persistence\CurrencyQueryContainer getQueryContainer()()
+ */
+class QuoteCurrencyValidatePlugin extends AbstractPlugin implements QuoteValidatePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     * - Validates if provided currency in quote is available.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return \Generated\Shared\Transfer\MessageTransfer
+     */
+    public function validate(QuoteTransfer $quoteTransfer): MessageTransfer
+    {
+        return $this->getFacade()->validateCurrencyInQuote($quoteTransfer);
+    }
+}
